f2d47270e22acb41a88ec113254287f7101b2241
Naihtton <whitediary1@outlook.com>
2021-11-30 20:04:11 +0700

    finished Lab7_3.cpp

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,15 +1,27 @@
 #include<iostream>
-
 using namespace std;
-
-
+int adiff(int A,int B){
+  int C = (B - A)%360;
+  if(C < -180){
+    C = C+360;
+  }
+  if(C > 180){
+    C = C-360;
+  }
+  if(C<0){
+    C = -C;
+  }
+  return C;
+}
 int main(){
-  cout << adiff(180,270);
-  cout << adiff(210,45);
-  cout << adiff(0,360);
-  cout << adiff(10,350);
-  cout << adiff(95,260);
-  cout << adiff(90,-90);
-  cout << adiff(1000,280);
+  cout << adiff(180,270) << endl;
+  cout << adiff(210,45)<< endl;
+  cout << adiff(0,360)<< endl;
+  cout << adiff(10,350)<< endl;
+  cout << adiff(95,260)<< endl;
+  cout << adiff(90,-90)<< endl;
+  cout << adiff(1000,280)<< endl;
   cout << adiff(60,244);
 }
+
+
